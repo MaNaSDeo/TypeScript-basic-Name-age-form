2caaa79f1aa80885340f11b229b5c770a6b87b5c
MaNaSDeo <rmnslogin@gmail.com>
2024-02-04 13:18:18 +0530

    Custom Form Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import { useRef } from "react";
+import Form from "./components/Form";
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  // const input = useRef<HTMLInputElement>(null);
 
   return (
     <main>
-      <Input label="Test" id="test" ref={input} />
+      <Form>
+        <Input label="Name" id="name" type="text" />
+        <Input label="Age" id="age" type="number" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
